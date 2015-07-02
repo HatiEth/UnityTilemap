5fb46d8cfa03a6006fd2f59c20fe71aff591ef1a
Hati Eth <hati.eth@gmail.com>
2015-07-02 15:48:56 +0200

    currently cleaning up previous test and preparing for release

--- Assets/Tilemap/Editor/TilemapEditor.cs
+++ Assets/Tilemap/Editor/TilemapEditor.cs
@@ -16,11 +16,14 @@ public class TilemapEditor : Editor {
     public void OnSceneGUI()
     {
         // EventHandling
+        Handles.DoPositionHandle(map.transform.position, Quaternion.identity);
     }
 
     [DrawGizmo(GizmoType.NonSelected|GizmoType.Active)]
     public static void OnDrawGizmo(Tilemap map, GizmoType type)
     {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(map.transform.position, new Vector3(10, 10, 10));
 
     }
 
